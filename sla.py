5a0edccbafd1f6d2474e7254f4314ea1c8af4f12
BlackAlexandre <159321029+BlackAlexandre@users.noreply.github.com>
2025-02-25 12:44:55 -0300

    Create sla.py

---
+++ sla.py
@@ -0,0 +1,12 @@
+a = float (input("Insira sua nota do primeiro trimestre:"))
+
+b = float (input("Insira sua nota do segundo trimestre:"))
+
+c = float (input("Insira sua nota do terceiro trimestre:"))
+
+d =(a + b + c) /3
+
+if d >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
